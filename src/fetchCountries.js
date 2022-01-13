9885acfb4e3d497570d7e45ca7455386d0c77e01
pylypchukroman <romanpylypchuk@gmail.com>
2022-01-13 22:17:03 +0200

    додає часткове рішення задачі

---
+++ src/fetchCountries.js
@@ -0,0 +1,11 @@
+export default  function fetchCountries(name)  {
+    const BASE_URL = "https://restcountries.com/v2/name/";
+    return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`).then((response) => {
+         if(!response.ok){
+              throw new Error(response.status);
+          }
+      return response.json();
+      })
+};
+
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
-console.log('12');
+// console.log(fetchCountries("peru"));
+
+
+const searchField = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list')
+
+
+searchField.addEventListener('input', debounce((countryInput), DEBOUNCE_DELAY));
+
+function countryInput(evt) {
+    const countryName = evt.target.value;
+    const normalCountryName = countryName.trim();
+   if(!normalCountryName){
+    countryList.innerHTML = "";
+    } else {
+        fetchCountries(normalCountryName)
+            .then(countries => {
+                if (countries.length > 10) {
+                    console.log("Too many matches found. Please enter a more specific name.");
+
+                } else if (countries.length >= 2 && countries.length <= 10) {
+                    console.log('знайдено декілька країн');
+                    console.log(countries);
+                } else {
+                    console.log('конкретна країна знайдена');
+                    console.log(countries);
+                }
+            })
+            .catch(error =>  console.log('Oops, there is no country with that name'));
+    }
+    }
